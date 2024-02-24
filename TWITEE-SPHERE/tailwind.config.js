76a65abdfdcdbd5c7ab605457f0e9dd939b16c63
Tristan vera <tristan.vera19@gmail.com>
2024-02-24 16:25:32 +0100

    ajouts des fichier tailwind

---
+++ TWITEE-SPHERE/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        blueLogo: '#2AA3EF'
+      },
+    },
+  },
+  plugins: [],
+}
+
