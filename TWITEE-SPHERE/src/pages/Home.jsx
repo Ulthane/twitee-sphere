b305db50e5d827933f899faf4051d9085d060910
delto <tbo.z@hotmail.fr>
2024-02-24 13:56:01 +0100

    bug fix react router dom

--- TWITEE-SPHERE/src/pages/Home.jsx
+++ TWITEE-SPHERE/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>Test</h1>
+      <h1 className="text-2xl">Test</h1>
+      <div className=" bg-red-300">BlablaBla</div>
     </>
   );
 }
