b80c4569ea28d08bb630b186ae5066779c67c310
delto <tbo.z@hotmail.fr>
2024-02-24 16:56:14 +0100

    add react router

--- TWITEE-SPHERE/src/App.jsx
+++ TWITEE-SPHERE/src/App.jsx
@@ -1,8 +1,45 @@
-import RegisterPage from "./pages/registerPage/RegisterPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-export default function App(){
+function App() {
+  const Main = lazy(() => import("./Layout/Main.jsx"));
+  const Home = lazy(() => import("./pages/Home.jsx"));
+  const RegisterPage = lazy(() => import("./pages/registerPage/RegisterPage"));
 
-  return(
-    <RegisterPage/>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Suspense>
+          <Main />
+        </Suspense>
+      ),
+      // errorElement: (
+      //   <Suspense>
+      //     <Error />
+      //   </Suspense>
+      // ),
+      children: [
+        {
+          index: true,
+          element: (
+            <Suspense>
+              <Home />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/RegisterPage",
+          element: (
+            <Suspense>
+              <RegisterPage />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
+
+export default App;
---
+++ TWITEE-SPHERE/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blueLogo: "#2AA3EF",
+      },
+    },
+  },
+  plugins: [],
+};
