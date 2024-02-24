d934d72ff1bd5fa575357503598a8597c2ce9d50
delto <tbo.z@hotmail.fr>
2024-02-24 16:31:55 +0100

    intialisation branch

---
+++ TWITEE-SPHERE/src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+
+export default function Main() {
+  return (
+    <>
+      {/* NavBar */}
+
+      {/* SideBar Menue */}
+
+      {/* SideBar Community */}
+
+      {/* Outlet */}
+      <Outlet />
+
+      {/* Footer */}
+    </>
+  );
+}
---
+++ TWITEE-SPHERE/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl">Test</h1>
+      <div className=" bg-red-300">BlablaBla</div>
+    </>
+  );
+}
