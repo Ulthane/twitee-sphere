8b2acdc933e24cff5778434a4cdf1de4e32012ba
Tristan vera <tristan.vera19@gmail.com>
2024-02-26 15:43:57 +0100

    ajout de la props diabled sur le composant

--- TWITEE-SPHERE/src/components/Button/Button.jsx
+++ TWITEE-SPHERE/src/components/Button/Button.jsx
@@ -3,6 +3,10 @@ import "./button.css";
 // composant bouton qui pourra être réutiliser partout
 // pour modifier le style du composant utiliser la props classname
 
-export default function Button({ value, className }) {
-  return <button className={`buttonCustom ${className}`}>{value}</button>;
+export default function Button({ value, className, disabled }) {
+  return (
+    <button disabled={disabled} className={`buttonCustom ${className}`}>
+      {value}
+    </button>
+  );
 }
