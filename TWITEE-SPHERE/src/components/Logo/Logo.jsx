3bf7d11f1dd7f33e350c7b9633b7657624e93b05
Tristan vera <tristan.vera19@gmail.com>
2024-02-26 17:59:27 +0100

    ajout d'un padding et agrandissement du text

--- TWITEE-SPHERE/src/components/Logo/Logo.jsx
+++ TWITEE-SPHERE/src/components/Logo/Logo.jsx
@@ -1,6 +1,6 @@
 export default function Logo() {
   return (
-    <h2 className="text-white">
+    <h2 className="text-white p-2 text-[1.6em]">
       Twitee<span className="text-blueLogo">Sphere</span>
     </h2>
   );
