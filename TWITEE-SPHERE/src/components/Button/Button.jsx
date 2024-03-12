24aeb5db1b0a32917e8379113fb20b492057ac09
Tristan vera <tristan.vera19@gmail.com>
2024-03-12 19:30:26 +0100

    ajout d'une props au button

--- TWITEE-SPHERE/src/components/Button/Button.jsx
+++ TWITEE-SPHERE/src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import "./button.css";
 // composant bouton qui pourra être réutiliser partout
 // pour modifier le style du composant utiliser la props classname
 
-export default function Button({ value, className, disabled, style }) {
+export default function Button({ value, className, disabled, style, click }) {
   return (
     <button
       disabled={disabled}
