e56f3e4ccb5d3626807cf702c14342a7dca937a8
Tristan vera <tristan.vera19@gmail.com>
2024-03-12 10:20:34 +0100

    Modification du composant Community

--- TWITEE-SPHERE/src/components/Community/Community.jsx
+++ TWITEE-SPHERE/src/components/Community/Community.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import { useToken } from "../../hook/useToke";
 import Button from "../Button/Button";
 
 export default function Community({ communitiesToDisplay }) {
@@ -8,24 +6,24 @@ export default function Community({ communitiesToDisplay }) {
       {communitiesToDisplay.map((community, index) => (
         <div
           key={index}
-          className="rounded-[25px] p-5 w-[700px] community mb-[15px]"
+          className="rounded-[25px] p-5 w-[600px] h-[auto] community mb-[15px]"
           style={{ background: "rgba(42, 163, 239, 0.1)" }}
         >
           {/* Mapping des communautés pour afficher dynamiquement */}
 
-          <div className="flex justify-between mb-[20px]">
+          <div className="flex justify-between mb-[5px]">
             <h3>profil ..</h3>
             <img
-              className="w-[40px] rounded-[10px]"
-              src="https://cdn.pixabay.com/photo/2018/05/26/18/06/dog-3431913_1280.jpg"
-              alt=""
+              className="w-[40px] h-[30px] object-contain rounded-[10px]"
+              src={community.icon}
+              alt="icone de la communauté"
             />
           </div>
           <h2 className="text-blueLogo text-[36px]">
             <b>{community.name}</b>
           </h2>
           <p className="text-[16px]">{community.description}</p>
-          <div className="mt-[35px] flex justify-between items-center">
+          <div className="mt-[15px] flex justify-between items-center">
             <div className="flex items-center">
               <img
                 className="mr-[10px]"
@@ -34,7 +32,12 @@ export default function Community({ communitiesToDisplay }) {
               />
               <p>2532</p>
             </div>
-            <Button value={"Rejoindre"} />
+            <Button
+              value={"Rejoindre"}
+              className={
+                "transition-all duration-200 ease-in-out hover:font-bold  hover:bg-blue-500"
+              }
+            />
           </div>
         </div>
       ))}
